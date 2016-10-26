7114432c2905134173c653b344dff9ba75c2ae1d
z1cc2xxx3 <alexzcx@bu.edu>
2016-10-26 19:02:32 -0400

    iOS App
    iOS App

---
+++ GodMode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GodMode
+//
+//  Created by 张晨曦 on 2016/10/26.
+//  Copyright © 2016年 张晨曦Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
